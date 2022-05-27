8eeda14caf053b511d5a36dea772fbb510fc9f00
Jacob Auger <jga9325@rit.edu>
2022-05-27 19:03:56 -0400

    New design and added comments

--- src/cell.py
+++ src/cell.py
@@ -1,36 +1,135 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
 import settings
+import ctypes
+import sys
 
+# Cell class represents a cell on the mine sweeper board
+# Each cell has a button so it can be clicked
 class Cell:
     all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_mine_candidate = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
+        self.has_been_clicked = False
 
         # Append the object to the Cell.all list
         Cell.all.append(self)
 
+     # Represents a list of cell objects that surround the cell
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x-1, self.y-1),
+            self.get_cell_by_axis(self.x-1, self.y),
+            self.get_cell_by_axis(self.x-1, self.y+1),
+            self.get_cell_by_axis(self.x, self.y-1),
+            self.get_cell_by_axis(self.x+1, self.y-1),
+            self.get_cell_by_axis(self.x+1, self.y),
+            self.get_cell_by_axis(self.x+1, self.y+1),
+            self.get_cell_by_axis(self.x, self.y+1),
+        ]
+        
+        cells = [cell for cell in cells if cell != None]
+        return cells
+
+    # Represents a count of mines around the cell
+    @property
+    def surrounded_cells_mine_count(self):
+        count = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                count += 1
+        return count
+
+    # Create a new button object for this cell and bind clicks to actions
     def create_btn_object(self, location):
         btn = Button(
             location,
             width=12,
-            height=4
+            height=4,
+            bg="#AFAFAF"
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
 
+    # Create a label for the count of cells left when playing the game
+    # It is static because it is a count for all of the cells not just one instance
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            text=f"Cells Left: {Cell.cell_count}",
+            bg="black",
+            fg="#AFAFAF",
+            font=("", 24)
+        )
+        Cell.cell_count_label_object = lbl
+
+    # Handle left clicks
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            if self.surrounded_cells_mine_count == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            self.show_cell()
+            # If the number of mines left is the same as the number of cells left,
+            # then the player wins
+            if Cell.cell_count == settings.MINES_COUNT:  
+                ctypes.windll.user32.MessageBoxW(0, 'Congratulations, you won the game!', 'Game Over', 0)
+                sys.exit()
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
 
+    # Return a cell object by providing its coordinates
+    def get_cell_by_axis(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    # Functionality for if a cell is left clicked
+    def show_cell(self):
+        if not self.has_been_clicked:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text = self.surrounded_cells_mine_count)
+            # Update cell count
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left: {Cell.cell_count}"
+                )
+            # Set original background color in case orange cell was clicked
+            self.cell_btn_object.configure(
+                bg="SystemButtonFace"
+            )
+        self.has_been_clicked = True
+
+    # Functionality for right clicks
+    def show_mine(self):
+        ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine', 'Game Over', 0)
+        sys.exit()
+
+    # Handles right clicks
     def right_click_actions(self, event):
-        print(event)
-        print("I am right clicked!")
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(
+                bg="orange"
+            )
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(
+                bg="#AFAFAF"
+            )
+            self.is_mine_candidate = False
 
+    # Chooses random cells to be mines
     @staticmethod
     def randomize_mines():
         picked_cells = random.sample(
@@ -39,5 +138,6 @@ class Cell:
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
 
+    # Returns a string version of the Cell class with its x and y coordinates
     def  __repr__(self):
         return f"Cell({self.x}, {self.y})"
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import settings
 import utils
 from cell import Cell
+from PIL import Image, ImageTk
 
 root = Tk()
 
@@ -11,6 +12,7 @@ root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Mine Sweeper")
 root.resizable(False, False)
 
+# Create a frame to hold the title
 top_frame = Frame(
     root,
     bg="black",
@@ -19,14 +21,27 @@ top_frame = Frame(
 )
 top_frame.place(x=0, y=0)
 
+# Create a title for the game
+game_title = Label(
+    top_frame,
+    bg="#00387A",
+    fg="white",
+    text="Mine Sweeper",
+    font=("", 42),
+    width=15
+)
+game_title.place(x=utils.width_prct(40.5),y=40)
+
+# Create a frame to hold game info and directions
 left_frame = Frame(
     root,
     bg="black",
-    width=utils.width_prct(25),
+    width=utils.width_prct(36),
     height=utils.height_prct(75)
 )
 left_frame.place(x=0, y=utils.height_prct(25))
 
+# Create a frame to hold the board
 center_frame = Frame(
     root,
     bg="black",
@@ -34,10 +49,11 @@ center_frame = Frame(
     height=utils.height_prct(75)
 )
 center_frame.place(
-    x=utils.width_prct(25),
+    x=utils.width_prct(37),
     y=utils.height_prct(25)
 )
 
+# Create the board and place it in the center frame
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
         c = Cell(x, y)
@@ -46,6 +62,61 @@ for x in range(settings.GRID_SIZE):
             column=x, row=y
         )
 
+# Create a new label in the Cell class
+Cell.create_cell_count_label(left_frame)
+Cell.cell_count_label_object.place(x=85, y=0)
+
+# Create a new label to display the total number of mines
+mines_count_display = Label(
+    left_frame,
+    bg="black",
+    fg="#9E0000",
+    text=f"Total Mines: {settings.MINES_COUNT}",
+    font=("", 24)
+)
+mines_count_display.place(x=80, y=60)
+
+# Add a label for the directions title
+directions_title = Label(
+    left_frame,
+    fg="white",
+    bg="black",
+    text="Directions",
+    font=("", 20, "underline")
+)
+directions_title.place(x=120, y=120)
+
+# Add a label for the directions text
+directions = Label(
+    left_frame,
+    bg="black",
+    fg="white",
+    text="""- Left click on a cell to reveal the number of\nmines around it (on all sides including\ndiagonally)\n
+- If you left click on a mine, you lose\n
+- Right click on a cell that you think is a mine to\nmark it orange\n
+- Click on all of the cells that are not mines in\norder to win the game\n
+- Good Luck!""",
+    font=("", 13),
+    justify="center"
+)
+directions.place(x=5, y=170)
+
+# Open image and resize it
+image = Image.open("src/explosion.png")
+resized_image = image.resize((150, 150), Image.ANTIALIAS)
+new_image = ImageTk.PhotoImage(resized_image)
+
+# Create a new label to place the image in the top frame
+pic = Label(
+    top_frame,
+    image=new_image,
+    width=150,
+    height=150,
+    bg="black"
+)
+pic.place(x=102, y=10)
+
+# Set random cells to become mines
 Cell.randomize_mines()
 
 # Run the window
